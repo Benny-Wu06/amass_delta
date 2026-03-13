cd7dc238709c99c49ed6bebaa89ea4ea27732042
Benny Wu <z5604774@ad.unsw.edu.au>
2026-03-14 04:02:03 +1100

    Terrafrom code for enrightment lambda

--- microservices/data_collection/terraform/reference.tf
+++ microservices/data_collection/terraform/reference.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "reference_scraper" {
   role             = aws_iam_role.cisa_role.arn
   handler          = "reference.nvdscrapper"
   runtime          = "python3.12"
-  timeout          = 300
+  timeout          = 600
   memory_size      = 3000
   filename         = data.archive_file.reference_zip.output_path
   source_code_hash = data.archive_file.reference_zip.output_base64sha256
