2fe308a3c07eca93c9fff7a4af447442150b9e76
Timothy Hidalgo <timer4075@gmail.com>
2026-03-14 00:16:27 +1100

    made sql query for getting company summary, refactor lambda to use separate function for the logic

--- microservices/data_retrieval/src/company_summary.py
+++ microservices/data_retrieval/src/company_summary.py
@@ -1,6 +1,7 @@
 import psycopg2
 import boto3
 import json
+from datetime import datetime
 
 password = "testdiddyblud"
 
@@ -26,11 +27,16 @@ def lambda_handler(event, context):
     path_params = event.get('pathParameters', {})
     target_company = path_params.get('company_name')
     
+    target_company = 'PedalDynamics'
     if not target_company:
         return {
             'statusCode': 400,
             'body': json.dumps({"error": "Company name is required in the URL"})
         }
+    get_company_summary(target_company)
+
+
+def get_company_summary(company):
     try:
         conn = psycopg2.connect(
             host='testdb.cjwhnekr8yms.us-east-1.rds.amazonaws.com',
@@ -43,30 +49,43 @@ def lambda_handler(event, context):
         sslrootcert='/certs/global-bundle.pem'
         )
         cur = conn.cursor()
-
-        # TODO WRITE QUERY
+        
         query = '''
             SELECT 
-                v.cve_id, 
-                c.company_name, 
-                v.vulnerability_name, 
-                v.cvss_score, 
-                v.cvss_severity
-            FROM vulnerabilities v
-            JOIN companies c ON v.company_id = c.id
-            WHERE c.company_name = %s;
+            c.company_name as company,
+            COUNT(v.cve_id) as cve_count,
+            AVG(v.epss_score) as avg_epss,
+            AVG(v.cvss_score) as avg_cvss,
+            c.risk_index,
+            c.risk_rating
+        FROM companies c
+        LEFT JOIN vulnerabilities v ON c.id = v.company_id
+        WHERE c.company_name = %s
+        GROUP BY c.id;
         '''
         
         cur.execute(query, (target_company,))
-
-        # transform result into list of dictionaries 
-        results = ['TODO']
+        row = cur.fetchone()
+        company, cve_count, avg_epss, avg_cvss, risk_index, risk_rating = row
+        avg_epss = float(avg_epss)
+        avg_cvss = float(avg_cvss)
+        risk_index = float(risk_index)
+        time_object = {'timestamp': str(datetime.now()), 'timezone': 'GMT+!11'}
+        
+        result = json.dumps({
+            'company': company,
+            'cve_count': cve_count,
+            'avg_epss': avg_epss,
+            'risk_index': risk_index,
+            'risk_rating': risk_rating,
+            'time': time_object
+        })
         cur.close()
 
         return {
             'statusCode': 200,
             'headers': {'Content-Type': 'application/json'},
-            'body': json.dumps(results)
+            'body': json.dumps(result)
         }
     except Exception as e:
         print(f"Database error: {e}")
@@ -78,3 +97,5 @@ def lambda_handler(event, context):
     finally:
         if conn:
             conn.close()
+
+get_company_summary(1,1)
