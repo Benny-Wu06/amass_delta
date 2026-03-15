16f3d1fe6da5ec002a721e3318f0869208e318c2
Dearryl Margalan <dearryllan@gmail.com>
2026-03-15 17:57:44 +1100

    Added all dependencies into the same file for terraform purposes

--- microservices/visualisation/src/cve_growth/cve_growth_lambda.py
+++ microservices/visualisation/src/cve_growth/cve_growth_lambda.py
@@ -1,7 +1,72 @@
 import json
-from db import get_db_connection
-from db_queries import fetch_company_name, fetch_vulnerability_data
-from cve_growth_processor import calculate_growth_stats
+import psycopg2
+import os
+from datetime import datetime, timedelta
+
+# --- DB CONFIGURATION CONSTANTS ---
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = 5432
+DB_NAME = os.environ.get('DB_NAME', 'postgres')
+DB_USER = os.environ.get('DB_USER', 'postgres')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_SSL_MODE = 'prefer'
+DB_SSL_ROOT_CERT = '/certs/global-bundle.pem'
+DB_TIMEOUT = 3
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD, 
+        sslmode=DB_SSL_MODE,
+        connect_timeout=DB_TIMEOUT,
+        sslrootcert=DB_SSL_ROOT_CERT
+    )
+
+def fetch_company_name(db, company_id):
+    db.execute("SELECT company_name FROM companies WHERE id = %s;", (company_id,))
+    row = db.fetchone()
+    return row[0] if row else None
+
+def fetch_vulnerability_data(db, company_id, days):
+    query = """
+        SELECT date_added, COUNT(*) as cve_count
+        FROM vulnerabilities
+        WHERE company_id = %s 
+        AND date_added >= CURRENT_DATE - (%s || ' days')::interval
+        GROUP BY date_added
+        ORDER BY date_added ASC;
+    """
+    db.execute(query, (company_id, days))
+    return db.fetchall()
+
+def calculate_growth_stats(vulnerability_counts, days, reference_date=None):
+    if reference_date is None:
+        reference_date = datetime.now()
+        
+    daily_counts = {str(entry[0]): entry[1] for entry in vulnerability_counts}
+    
+    data_points = []
+    total_increase = 0
+    peak_val = -1
+    peak_day = "N/A"
+    
+    for i in range(days - 1, -1, -1):
+        target_date = (reference_date - timedelta(days=i)).strftime("%Y-%m-%d")
+        count = daily_counts.get(target_date, 0)
+        
+        data_points.append({"date": target_date, "new_cves": count})
+        
+        total_increase += count
+        if count > peak_val:
+            peak_val = count
+            peak_day = target_date
+            
+    return data_points, total_increase, peak_day
+
+############## LAMBDA FUNCTION ###############
 
 def cve_growth_lambda(event, context):
     try:
