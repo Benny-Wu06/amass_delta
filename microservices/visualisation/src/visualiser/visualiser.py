5f02d6b351ab159404ae67c30e847748ba5ec71b
Dearryl Margalan <dearryllan@gmail.com>
2026-03-15 16:54:22 +1100

    refactor: separated the graph generation logic, made helper functions (strategy pattern)

--- microservices/visualisation/src/visualiser/visualiser.py
+++ microservices/visualisation/src/visualiser/visualiser.py
@@ -16,34 +16,10 @@ plt.rcParams.update({
 def generate_plot_bytes(data):
     buf = io.BytesIO()
 
-    # HEATMAP
     if "heatmap_grid" in data:
-        df_heat = pd.DataFrame(data["heatmap_grid"])
-        pivot_table = df_heat.pivot(index="epss", columns="cvss", values="count").fillna(0)
-        cvss_order = ["0-2", "2-4", "4-6", "6-8", "8-10"]
-        epss_order = ["0-0.2", "0.2-0.4", "0.4-0.6", "0.6-0.8", "0.8-1.0"]
-        pivot_table = pivot_table.reindex(index=epss_order[::-1], columns=cvss_order).fillna(0)
-
-        plt.figure(figsize=(9, 7))
-        ax = sns.heatmap(pivot_table, annot=True, cmap="Reds", fmt='g')
-        ax.set_xlabel("CVSS Score", fontweight='bold')
-        ax.set_ylabel("EPSS Score", fontweight='bold')
-        plt.title(f"{data.get('company_name', 'Company')}: Risk Heatmap", pad=20, fontweight='bold')
-
-    # LINE GRAPH
+        generate_heatmap(data)
     elif "data_points" in data:
-        df_line = pd.DataFrame(data['data_points'])
-        summary = data['summary']
-        plt.figure(figsize=(10, 6))
-        ax = sns.lineplot(data=df_line, x='date', y='new_cves', marker='o', linewidth=2.5, color='#d62708')
-        
-        ax.set_xlabel("Observation Date", fontweight='bold', labelpad=10)
-        ax.set_ylabel("New CVEs Reported", fontweight='bold', labelpad=10)
-        plt.title(f"CVE Growth Trend: {data.get('company_name')}", fontsize=16, pad=20, fontweight='bold')
-        
-        stats_box = (f"Total Increase: {summary['total_period_increase']}\nPeak: {summary['peak_growth_day']}")
-        plt.gca().annotate(stats_box, xy=(0.02, 0.9), xycoords='axes fraction', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.8))
-        plt.tight_layout()
+        generate_line_graph(data)
     else:
         raise ValueError("Unknown data format: missing required keys")
 
@@ -51,3 +27,34 @@ def generate_plot_bytes(data):
     plt.close()
     buf.seek(0)
     return buf.read()
+
+############################## GRAPH GENERATION HELPERS #################################
+
+def generate_heatmap(data):
+    df_heat = pd.DataFrame(data["heatmap_grid"])
+    pivot_table = df_heat.pivot(index="epss", columns="cvss", values="count").fillna(0)
+    cvss_order = ["0-2", "2-4", "4-6", "6-8", "8-10"]
+    epss_order = ["0-0.2", "0.2-0.4", "0.4-0.6", "0.6-0.8", "0.8-1.0"]
+    pivot_table = pivot_table.reindex(index=epss_order[::-1], columns=cvss_order).fillna(0)
+
+    plt.figure(figsize=(9, 7))
+    ax = sns.heatmap(pivot_table, annot=True, cmap="Reds", fmt='g')
+    ax.set_xlabel("CVSS Score", fontweight='bold')
+    ax.set_ylabel("EPSS Score", fontweight='bold')
+    plt.title(f"{data.get('company_name', 'Company')}: Risk Heatmap", pad=20, fontweight='bold')
+
+def generate_line_graph(data):
+    df_line = pd.DataFrame(data['data_points'])
+    summary = data['summary']
+    plt.figure(figsize=(10, 6))
+    ax = sns.lineplot(data=df_line, x='date', y='new_cves', marker='o', linewidth=2.5, color='#d62708')
+    
+    ax.set_xlabel("Observation Date", fontweight='bold', labelpad=10)
+    ax.set_ylabel("New CVEs Reported", fontweight='bold', labelpad=10)
+    plt.title(f"CVE Growth Trend: {data.get('company_name')}", fontsize=16, pad=20, fontweight='bold')
+    
+    stats_box = (f"Total Increase: {summary['total_period_increase']}\nPeak: {summary['peak_growth_day']}")
+    plt.gca().annotate(stats_box, xy=(0.02, 0.9), xycoords='axes fraction', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.8))
+    plt.tight_layout()
+
+###########################################################################################
