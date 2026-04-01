fcb9768483708b23dd89bd37daba357255965293
Timothy Hidalgo <timer4075@gmail.com>
2026-04-02 02:13:09 +1100

    add db connection + seeding fixtures

--- microservices/data_retrieval/tests/integration/test_int_vuln_info.py
+++ microservices/data_retrieval/tests/integration/test_int_vuln_info.py
@@ -31,21 +31,60 @@ def lambda_client():
     # so when called from staging branch, should have the right access.
     return boto3.client("lambda")
 
-def test_lambda_success(lambda_client):
+# connect to db
+def conn_db(scope="module"):
+    conn = None
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_HOST = os.environ.get("DB_HOST")
+    cert_path = os.environ.get("CERT_PATH", "global-bundle.pem")
+
+    conn = psycopg2.connect(
+            host=DB_HOST,
+            port=5432,
+            database=os.environ.get("DB_NAME", "postgres"),
+            user=os.environ.get("DB_USER", "postgres"),
+            password=DB_PASSWORD,
+            sslmode="require",
+            connect_timeout=50,
+            sslrootcert=cert_path,
+    )
+    yield conn
+    conn.close()
+            
+
+# seed db, this sets up and tears down the vulnerability for every test
+def seed_db(scope="function"):
+    cur = conn_db.cursor()
+
+    query = """
+        insert into vulnerabilities (cve_id, company_id, vulnerability_name, description,
+          date_added, due_date, cvss_score, cvss_severity, epss_score, epss_percentile)
+        values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    cur.execute(query, (CVE_ID, COMPANY_ID, VULN_NAME, DESC, DATE_ADDED, DUE_DATE, CVSS, CVSS_SEVERITY, EPSS, EPSS_PERCENTILE,))
+    cur.commit()
+
+    yield
+
+    delete_query = "DELETE FROM vulnerabilities WHERE cve_id = %s;"
+    cur.execute(delete_query, (CVE_ID,))
+    cur.commit()
+    cur.close()
+
+def test_vuln_retrieval_success(lambda_client):
     event = {
         "pathParameters": {
-            "cve_id": "CVE-2021-22175"
+            "cve_id": CVE_ID
         }
     }
     
     response = lambda_client.invoke(
         FunctionName='vuln_info_lambda',
-  InvocationType="RequestResponse",
+        InvocationType="RequestResponse",
         Payload=json.dumps(event)
     )
     
     response_payload = json.loads(response["Payload"].read().decode("utf-8"))
-    
     assert response_payload["statusCode"] == 200
     
     body = json.loads(response_payload["body"])
