0bd54017286348e561527ac02951f37a3b337d4e
Dearryl Margalan <dearryllan@gmail.com>
2026-04-08 17:38:56 +1000

    implemented main to initialise FastAPI for auth services

---
+++ microservices/auth/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routes import router as auth_router
+
+# init FastAPI
+app = FastAPI(
+    title="Amass Delta Auth Service",
+    version="1.0.0"
+)
+
+app.include_router(auth_router)
