abba3ff3fc242907a5d7c7835f017ef1b39984e4
Timothy Hidalgo <timer4075@gmail.com>
2026-04-08 18:55:14 +1000

    set db to publically accessible

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,7 +88,7 @@ resource "aws_db_instance" "postgres" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
-  publicly_accessible = false
+  publicly_accessible = true
   skip_final_snapshot = true
 }
 
