4bcbcc72ba7344c6892f9b0dfe91022ef4d5a977
william <z5560966@ad.unsw.edu.au>
2026-04-10 16:24:07 +1000

    draft get all companies and terraform

---
+++ microservices/data_retrieval/package/get_all_companies.py
@@ -0,0 +1,59 @@
+import logging
+import psycopg2
+import json
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import os
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    return get_all_companies()
+
+
+def get_all_companies():
+    conn = None
+    try:
+        DB_PASSWORD = os.environ.get("DB_PASSWORD")
+        DB_HOST = os.environ.get("DB_HOST")
+        cert_path = os.environ.get("CERT_PATH", "global-bundle.pem")
+
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            port=5432,
+            database=os.environ.get("DB_NAME", "postgres"),
+            user=os.environ.get("DB_USER", "postgres"),
+            password=DB_PASSWORD,
+            sslmode="require",
+            connect_timeout=50,
+            sslrootcert=cert_path,
+        )
+        cur = conn.cursor()
+
+        # Query to get all unique company names
+        query = "SELECT DISTINCT company_name FROM companies ORDER BY company_name ASC;"
+        cur.execute(query)
+        
+
+        rows = cur.fetchall()
+        
+        companies_list = [row[0] for row in rows]
+
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({
+                "count": len(companies_list),
+                "companies": companies_list
+            }),
+        }
+    except Exception as e:
+        logger.error("Failed to fetch company list: %s", str(e))
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": "Failed to retrieve company list"}),
+        }
+    finally:
+        if conn:
+            conn.close()
--- microservices/data_retrieval/terraform/main.tf
+++ microservices/data_retrieval/terraform/main.tf
@@ -71,6 +71,36 @@ resource "aws_lambda_function" "company_vulnerabilities" {
 
 }
 
+#
+# Get all companies lambda
+#
+resource "aws_lambda_function" "get_all_companies" {
+    function_name = "get_all_companies_service"
+    role          = aws_iam_role.lambda_exec_role.arn
+    handler       = "get_all_companies.lambda_handler"
+    runtime       = "python3.12"
+
+    filename         = data.archive_file.get_all_companies_zip.output_path
+    source_code_hash = data.archive_file.get_all_companies_zip.output_base64sha256
+
+    vpc_config {
+        subnet_ids         = var.private_subnet_ids
+        security_group_ids = [var.lambda_sg_id]
+    }
+
+    layers  = ["arn:aws:lambda:ap-southeast-2:580247275435:layer:LambdaInsightsExtension:21",
+    "arn:aws:lambda:ap-southeast-2:770693421928:layer:Klayers-p312-psycopg2-binary:1"]
+    environment {
+        variables = {
+        DB_HOST     = var.db_address
+        DB_NAME     = var.db_name
+        DB_USER     = var.db_user
+        DB_PASSWORD = var.db_password
+        CERT_PATH   = "global-bundle.pem"
+            }
+        }
+}
+
 
 #
 #  API Gateway Integration
@@ -94,3 +124,26 @@ resource "aws_lambda_permission" "company_vulnerabilities_perm" {
   source_arn    = "${var.api_execution_arn}/*/*"
 }
 
+#
+# get_all_companies
+#
+resource "aws_apigatewayv2_integration" "get_all_companies_int" {
+  api_id           = var.api_id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.get_all_companies.invoke_arn
+}
+
+
+resource "aws_apigatewayv2_route" "get_all_companies_route" {
+  api_id    = var.api_id
+  route_key = "GET /v1/companies"
+  target    = "integrations/${aws_apigatewayv2_integration.get_all_companies_int.id}"
+}
+
+
+resource "aws_lambda_permission" "get_all_companies_perm" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_all_companies.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${var.api_execution_arn}/*/*"
+}
