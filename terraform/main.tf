61ec0def3b2a3ad30107b0f763d41664aba36783
Timothy Hidalgo <timer4075@gmail.com>
2026-04-11 22:56:49 +1000

    add dependency to terraform db to wait before igw is attached to vpc before doing db stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,6 +128,11 @@ resource "aws_db_instance" "postgres" {
 
   publicly_accessible = true
   skip_final_snapshot = true
+  
+  # don't setup db until igw is attached to vpc
+  depends_on = [
+    aws_internet_gateway.amass_igw
+  ]
 }
 
 variable "db_password" {
