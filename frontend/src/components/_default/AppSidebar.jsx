69b32997422fdf9c5b9223f1e012b3b9f4fef59c
Timothy Hidalgo <timer4075@gmail.com>
2026-04-14 17:30:56 +1000

    add default components into /_default folder

--- frontend/src/components/_default/AppSidebar.jsx
+++ frontend/src/components/_default/AppSidebar.jsx
@@ -38,7 +38,7 @@ import { logo } from 'src/assets/brand/logo'
 import { sygnet } from 'src/assets/brand/sygnet'
 
 // sidebar nav config
-import navigation from '../_nav'
+import navigation from '../../_nav'
 
 /**
  * AppSidebar functional component
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,13 +1,13 @@
-import AppBreadcrumb from './AppBreadcrumb'
-import AppContent from './AppContent'
-import AppFooter from './AppFooter'
-import AppHeader from './AppHeader'
+import AppBreadcrumb from './_default/AppBreadcrumb'
+import AppContent from './_default/AppContent'
+import AppFooter from './_default/AppFooter'
+import AppHeader from './_default/AppHeader'
 import AppHeaderDropdown from './header/AppHeaderDropdown'
-import AppSidebar from './AppSidebar'
-import DocsComponents from './DocsComponents'
-import DocsIcons from './DocsIcons'
-import DocsLink from './DocsLink'
-import DocsExample from './DocsExample'
+import AppSidebar from './_default/AppSidebar'
+import DocsComponents from './_default/DocsComponents'
+import DocsIcons from './_default/DocsIcons'
+import DocsLink from './_default/DocsLink'
+import DocsExample from './_default/DocsExample'
 
 export {
   AppBreadcrumb,
