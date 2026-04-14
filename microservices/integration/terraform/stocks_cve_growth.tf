52100e855b1b07c58d10711b3f1c67b4ee131d93
Dearryl Margalan <dearryllan@gmail.com>
2026-04-14 23:33:03 +1000

    feat: created terraform files for stocks_cve_growth

---
+++ microservices/integration/terraform/stocks_cve_growth.tf
@@ -0,0 +1,84 @@
+data "archive_file" "stocks_cve_growth_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../stocks_cve_growth_lambda.py"
+  output_path = "${path.module}/stocks_cve.zip"
+}
+
+resource "aws_security_group" "integration_sg" {
+  name   = "integration-lambda-sg"
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "integration_role" {
+  name = "integration_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "stocks_cve_growth_lambda" {
+  filename      = data.archive_file.stocks_cve_growth_zip.output_path
+  function_name = "integration_stocks_cve_growth"
+  role          = aws_iam_role.integration_role.arn
+  handler       = "stocks_cve_growth_lambda.stocks_cve_growth_lambda"
+  runtime       = "python3.12"
+  timeout       = 30
+
+  layers = [
+    "arn:aws:lambda:ap-southeast-2:770693421928:layer:Klayers-p312-psycopg2-binary:1",
+    "arn:aws:lambda:ap-southeast-2:580247275435:layer:LambdaInsightsExtension:21",
+    "arn:aws:lambda:ap-southeast-2:770693421928:layer:Klayers-p312-requests:4"
+  ]
+
+  source_code_hash = data.archive_file.stocks_cve_growth_zip.output_base64sha256
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = [aws_security_group.integration_sg.id]
+  }
+
+  environment {
+    variables = {
+      DB_HOST          = var.db_address
+      DB_NAME          = var.db_name
+      DB_USER          = var.db_user
+      DB_PASSWORD      = var.db_password
+      CHARLIE_EMAIL    = var.charlie_email
+      CHARLIE_PASSWORD = var.charlie_password
+    }
+  }
+}
+
+resource "aws_apigatewayv2_integration" "stocks_cve_growth_lambda_int" {
+  api_id           = var.api_id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.stocks_cve_growth_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "stocks_cve_route" {
+  api_id    = var.api_id
+  route_key = "GET /v1/integration/{company_symbol}"
+  target    = "integrations/${aws_apigatewayv2_integration.stocks_cve_growth_lambda_int.id}"
+}
+
+resource "aws_lambda_permission" "stocks_cve_api_gw_perm" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.stocks_cve_growth_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${var.api_execution_arn}/*/*"
+}
---
+++ microservices/integration/terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "charlie_email" {
+  type = string 
+}
+
+variable "charlie_password" {
+  type = string
+  sensitive = true
+}
