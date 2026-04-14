9a8de561e3ebff4c301772e6189cb307b889524d
Timothy Hidalgo <timer4075@gmail.com>
2026-04-14 21:29:42 +1000

    remove newline that was annoying me

--- terraform/main.tf
+++ terraform/main.tf
@@ -148,7 +148,6 @@ resource "aws_apigatewayv2_api" "unified_api" {
   name          = "amass-unified-api"
   protocol_type = "HTTP"
 
-
   cors_configuration {
     allow_origins = ["*"]
     allow_methods = ["GET", "OPTIONS", "POST", "PUT", "DELETE"]
