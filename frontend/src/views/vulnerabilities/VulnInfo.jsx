661096d2795a36a1125bade19e6bef039bcd76f8
Timothy Hidalgo <timer4075@gmail.com>
2026-04-17 02:49:28 +1000

    add fetching cve info

--- frontend/src/views/vulnerabilities/VulnInfo.jsx
+++ frontend/src/views/vulnerabilities/VulnInfo.jsx
@@ -1,13 +1,25 @@
 import { useFetcher, useParams } from "react-router-dom"
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import { STAGING_URL } from '../../vars'
 
 const VulnInfo = ({}) => {
   const {cveId} = useParams()
   const [cveInfo, setCveInfo] = useState(null)
 
   useEffect(() => {
+    const fetchCVE = async () => {
+      try {
+        const response = await axios.get(`${STAGING_URL}/v1/vulnerabilities/${cveId}`)
+        setCveData(response.data)
+        console.log(response.data)
+      } catch (error) {
+        console.error('Failed to fetch CVE:', error)
+      }
+    }
+    fetchCVE()
+  }, [cveId])
 
-  }, [cveId]) 
-  
   return (
     <>
       <h1>{cveId}</h1>
