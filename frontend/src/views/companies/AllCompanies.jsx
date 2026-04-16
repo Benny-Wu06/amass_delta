a3ab3409933409915a04d3e2d524afd429f09956
Timothy Hidalgo <timer4075@gmail.com>
2026-04-16 23:26:30 +1000

    add view button to go to a specific company page

--- frontend/src/views/companies/AllCompanies.jsx
+++ frontend/src/views/companies/AllCompanies.jsx
@@ -55,7 +55,7 @@ const AllCompanies = () => {
               <div>Companies ({numCompanies})</div>
               <div>Sort by: TODO</div>
               <div>TODO SEARCH bAR</div>
-              </CCardHeader>
+            </CCardHeader>
             <CTable align="middle" className="mb-0 border" hover responsive>
               <CTableHead className="text-nowrap">
                 <CTableRow>
--- frontend/src/views/companies/CompanyRow.jsx
+++ frontend/src/views/companies/CompanyRow.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { CTableRow, CTableDataCell, CButton } from '@coreui/react'
 import AddCompanyDropdown from './AddCompanyDropdown.jsx'
+import { useNavigate } from 'react-router-dom'
 
 const CompanyRow = ({ companyName }) => {
+  const navigate = useNavigate()
+
+  // const companyId = getCompany().id or something
+
   return (
     <>
       <CTableRow>
         <CTableDataCell className="text-center">{companyName}</CTableDataCell>
         <CTableDataCell className="text-center">
-        <AddCompanyDropdown></AddCompanyDropdown>
+          <CButton color="primary" onClick={() => {navigate(`/companies/${companyName}`)}}>
+            View
+          </CButton>
+          <AddCompanyDropdown></AddCompanyDropdown>
         </CTableDataCell>
       </CTableRow>
     </>
