9123b67acd8a080d624d096f23e576a7a1de7c8e
Dearryl Margalan <dearryllan@gmail.com>
2026-04-17 21:22:51 +1000

    commented out

--- microservices/auth/terraform/api_gateway.tf
+++ microservices/auth/terraform/api_gateway.tf
@@ -1,52 +1,52 @@
-# creates api gateway
-resource "aws_apigatewayv2_api" "auth_api" {
-  name          = "auth-service-api"
-  protocol_type = "HTTP"
+# # creates api gateway
+# resource "aws_apigatewayv2_api" "auth_api" {
+#   name          = "auth-service-api"
+#   protocol_type = "HTTP"
 
-  cors_configuration {
-    allow_origins = ["*"]
-    allow_methods = ["GET", "POST", "OPTIONS"]
-    allow_headers = ["Content-Type", "Authorization"]
-  }
-}
+#   cors_configuration {
+#     allow_origins = ["*"]
+#     allow_methods = ["GET", "POST", "OPTIONS"]
+#     allow_headers = ["Content-Type", "Authorization"]
+#   }
+# }
 
-# automatic deploy changes 
-resource "aws_apigatewayv2_stage" "auth_stage" {
-  api_id      = aws_apigatewayv2_api.auth_api.id
-  name        = "$default"
-  auto_deploy = true
-}
+# # automatic deploy changes 
+# resource "aws_apigatewayv2_stage" "auth_stage" {
+#   api_id      = aws_apigatewayv2_api.auth_api.id
+#   name        = "$default"
+#   auto_deploy = true
+# }
 
-# connects api gateway to your lambda
-resource "aws_apigatewayv2_integration" "auth_integration" {
-  api_id                 = aws_apigatewayv2_api.auth_api.id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.auth_lambda.invoke_arn
-  payload_format_version = "2.0"
-}
+# # connects api gateway to your lambda
+# resource "aws_apigatewayv2_integration" "auth_integration" {
+#   api_id                 = aws_apigatewayv2_api.auth_api.id
+#   integration_type       = "AWS_PROXY"
+#   integration_uri        = aws_lambda_function.auth_lambda.invoke_arn
+#   payload_format_version = "2.0"
+# }
 
-# route for signup
-resource "aws_apigatewayv2_route" "signup" {
-  api_id    = aws_apigatewayv2_api.auth_api.id
-  route_key = "POST /auth/signup"
-  target    = "integrations/${aws_apigatewayv2_integration.auth_integration.id}"
-}
+# # route for signup
+# resource "aws_apigatewayv2_route" "signup" {
+#   api_id    = aws_apigatewayv2_api.auth_api.id
+#   route_key = "POST /auth/signup"
+#   target    = "integrations/${aws_apigatewayv2_integration.auth_integration.id}"
+# }
 
-# route for login
-resource "aws_apigatewayv2_route" "login" {
-  api_id    = aws_apigatewayv2_api.auth_api.id
-  route_key = "POST /auth/login"
-  target    = "integrations/${aws_apigatewayv2_integration.auth_integration.id}"
-}
+# # route for login
+# resource "aws_apigatewayv2_route" "login" {
+#   api_id    = aws_apigatewayv2_api.auth_api.id
+#   route_key = "POST /auth/login"
+#   target    = "integrations/${aws_apigatewayv2_integration.auth_integration.id}"
+# }
 
-# route for logout
-resource "aws_apigatewayv2_route" "logout" {
-  api_id    = aws_apigatewayv2_api.auth_api.id
-  route_key = "POST /auth/logout"
-  target    = "integrations/${aws_apigatewayv2_integration.auth_integration.id}"
-}
+# # route for logout
+# resource "aws_apigatewayv2_route" "logout" {
+#   api_id    = aws_apigatewayv2_api.auth_api.id
+#   route_key = "POST /auth/logout"
+#   target    = "integrations/${aws_apigatewayv2_integration.auth_integration.id}"
+# }
 
-# rrints the URl after deployment
-output "auth_api_url" {
-  value = aws_apigatewayv2_stage.auth_stage.invoke_url
-}
+# # rrints the URl after deployment
+# output "auth_api_url" {
+#   value = aws_apigatewayv2_stage.auth_stage.invoke_url
+# }
