a7d062aaf368afb9d13db37f1466fb7aeb681fed
Dearryl Margalan <dearryllan@gmail.com>
2026-04-17 20:59:11 +1000

    uncommented

--- microservices/auth/terraform/provider.tf
+++ microservices/auth/terraform/provider.tf
@@ -1,13 +1,13 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#         source = "hashicorp/aws"
-#         version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+}
 
 
-# provider "aws" {
-#   region = "ap-southeast-2"
-# }
+provider "aws" {
+  region = "ap-southeast-2"
+}
