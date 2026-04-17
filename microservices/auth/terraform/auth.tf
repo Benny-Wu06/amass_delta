68855d8c4046225784998028f691a72fcdd9dd53
Dearryl Margalan <dearryllan@gmail.com>
2026-04-17 21:24:12 +1000

    fix: fixed up some terraform problems

--- microservices/auth/terraform/auth.tf
+++ microservices/auth/terraform/auth.tf
@@ -5,6 +5,38 @@ data "archive_file" "auth_zip" {
   excludes    = ["terraform", "tests", "__pycache__", ".gitignore"]
 }
 
+resource "aws_security_group" "auth_sg" {
+  name   = "auth-lambda-sg"
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "auth_role" {
+  name = "auth_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.integration_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 resource "aws_lambda_function" "auth_lambda" {
   filename         = data.archive_file.auth_zip.output_path
   function_name    = "auth-service"
@@ -13,6 +45,17 @@ resource "aws_lambda_function" "auth_lambda" {
   runtime          = "python3.11"
   timeout          = 30
   source_code_hash = data.archive_file.auth_zip.output_base64sha256
+
+  layers = [
+    "arn:aws:lambda:ap-southeast-2:770693421928:layer:Klayers-p312-psycopg2-binary:1",
+    "arn:aws:lambda:ap-southeast-2:580247275435:layer:LambdaInsightsExtension:21",
+    "arn:aws:lambda:ap-southeast-2:770693421928:layer:Klayers-p312-requests:23"
+  ]
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = [aws_security_group.integration_sg.id]
+  }
 
   environment {
     variables = {
@@ -23,6 +66,13 @@ resource "aws_lambda_function" "auth_lambda" {
       JWT_SECRET  = var.jwt_secret
     }
   }
+}
+
+resource "aws_apigatewayv2_integration" "auth_integration" {
+  api_id                 = var.api_id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.auth_lambda.invoke_arn
+  payload_format_version = "2.0"
 }
 
 # Signup
@@ -51,11 +101,4 @@ resource "aws_lambda_permission" "api_gateway_auth" {
   function_name = aws_lambda_function.auth_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${var.api_execution_arn}/*/*"
-}
-
-resource "aws_apigatewayv2_integration" "auth_integration" {
-  api_id                 = var.api_id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.auth_lambda.invoke_arn
-  payload_format_version = "2.0"
 }
