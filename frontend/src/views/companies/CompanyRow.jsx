d9961c0c2d1be0de88b59693a02e12551005ba1b
Timothy Hidalgo <timer4075@gmail.com>
2026-04-21 16:28:30 +1000

    remove view button change whole company row to lead to the copmany info page

--- frontend/src/views/companies/CompanyRow.jsx
+++ frontend/src/views/companies/CompanyRow.jsx
@@ -1,30 +1,18 @@
 import React from 'react'
-import { CTableRow, CTableDataCell, CButton } from '@coreui/react'
+import { CTableRow, CTableDataCell } from '@coreui/react'
 import AddCompanyDropdown from './AddCompanyDropdown.jsx'
 import { useNavigate } from 'react-router-dom'
 
 const CompanyRow = ({ companyName }) => {
   const navigate = useNavigate()
 
-  // const companyId = getCompany().id or something
-
   return (
-    <>
-      <CTableRow>
-        <CTableDataCell className='mx-4'>{companyName}</CTableDataCell>
-        <CTableDataCell className="text-center">
-          <CButton className='mx-2'
-            color="primary"
-            onClick={() => {
-              navigate(`/companies/${companyName}`)
-            }}
-          >
-            View
-          </CButton>
-          <AddCompanyDropdown companyName={companyName} />
-        </CTableDataCell>
-      </CTableRow>
-    </>
+    <CTableRow style={{ cursor: 'pointer' }} onClick={() => navigate(`/companies/${companyName}`)}>
+      <CTableDataCell className="mx-4">{companyName}</CTableDataCell>
+      <CTableDataCell className="text-center" onClick={(e) => e.stopPropagation()}>
+        <AddCompanyDropdown companyName={companyName} />
+      </CTableDataCell>
+    </CTableRow>
   )
 }
 
